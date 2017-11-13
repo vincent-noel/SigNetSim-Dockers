962fb840f1744b97dbcbc9f4a7c4f4cfedc47147
vincent <vincent.noel@butantan.gov.br>
2017-11-13 14:13:30 -0200

    Added a script for choosing the image to build. Removed pip install verbosity

---
+++ build_images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $1 = "signetsim" ]; then
+
+    docker build -t signetsim/signetsim signetsim || exit 1;
+    docker commit signetsim/signetsim || exit 1;
+    docker push signetsim/signetsim || exit 1;
+
+elif [ $1 = "notebook" ]; then
+
+    docker build -t signetsim/notebook jupyter-notebook || exit 1;
+    docker commit signetsim/notebook || exit 1;
+    docker push signetsim/notebook || exit 1;
+
+else
+
+    docker build -t signetsim/travis_testenv:$1 $1 || exit 1;
+    docker commit signetsim/travis_testenv:$1 || exit 1;
+    docker push signetsim/travis_testenv:$1 || exit 1;
+
+fi
+
