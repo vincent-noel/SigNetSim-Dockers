c32eb33bf21f1b425c011d6f63936a4d4398ea48
vincent <vincent.noel@butantan.gov.br>
2017-11-13 18:50:05 -0200

    Added a development version

--- build_images.sh
+++ build_images.sh
@@ -5,6 +5,11 @@ if [ $1 = "signetsim" ]; then
     docker build -t signetsim/signetsim signetsim || exit 1;
     docker push signetsim/signetsim || exit 1;
 
+elif [ $1 = "develop" ]; then
+
+    docker build -t signetsim/signetsim:develop develop || exit 1;
+    docker push signetsim/signetsim:develop || exit 1;
+
 elif [ $1 = "notebook" ]; then
 
     docker build -t signetsim/notebook jupyter-notebook || exit 1;
